bb622370d4fe4e5389e057ae2c42e398bcc92c82
Duc Hoang Nguyen <lazycoder97@gmail.com>
2018-01-25 07:50:16 +0700

    add template for lab 2 to 7

---
+++ lab3/mainfile.cpp
@@ -0,0 +1,30 @@
+#include <gl/gl.h>
+#include <gl/glut.h>
+
+void myInit()  {
+    glClearColor(1, 1, 1, 0);
+    glMatrixMode( GL_PROJECTION );
+    glLoadIdentity( );
+    glEnable(GL_LINE_SMOOTH);
+    gluOrtho2D(0, 640, 0, 480);
+}
+
+void myDisplay()  {
+    // Clear
+    glClear( GL_COLOR_BUFFER_BIT );
+
+    // Draw
+
+    // Flush
+    glFlush();
+}
+
+int main( int argc, char *argv[] )  {
+    glutInit( &argc, argv );
+    glutInitDisplayMode( GLUT_SINGLE | GLUT_RGB );
+    glutInitWindowSize( 640, 480 );
+    glutCreateWindow( "Lab3" );
+    myInit( );
+    glutDisplayFunc( myDisplay );
+    glutMainLoop( );
+}
